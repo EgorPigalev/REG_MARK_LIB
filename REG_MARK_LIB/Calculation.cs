55e772369d1e4f25da0859f8bb5967857f9e7681
hhhEEElll <rtuezptim@mail.ru>
2023-02-19 19:48:45 +0300

    Доработки

--- REG_MARK_LIB/Calculation.cs
+++ REG_MARK_LIB/Calculation.cs
@@ -11,42 +11,188 @@ namespace REG_MARK_LIB
     {
         public static bool CheckMark(string mark) // Проверяет правильность номерного знака 
         {
-            Regex regex = new Regex("^[ABEKMHOPCTYX]{1}[0-9]{3}[ABEKMHOPCTYX]{2}[0-9]{3}");
-            bool a = regex.IsMatch(mark);
-            if(!a)
+            try
             {
-                return false;
-            }
-            else
-            {
-                string kodRegion = mark.Substring(mark.Length - 3);
-                if(kodRegion == "000")
+                Regex regex = new Regex("^[ABEKMHOPCTYX]{1}[0-9]{3}[ABEKMHOPCTYX]{2}[0-9]{3}");
+                bool a = regex.IsMatch(mark);
+                if (!a)
                 {
                     return false;
                 }
                 else
                 {
-                    return true;
+                    string nomer = mark.Substring(1, mark.Length - 6);
+                    string kodRegion = mark.Substring(mark.Length - 3);
+                    if (kodRegion == "000" || nomer == "000")
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
                 }
             }
+            catch
+            {
+                return false;
+            }
         }
+        public static List<char> symbolsUsed = new List<char>() {'A', 'B', 'E', 'K', 'M', 'H', 'O', 'P', 'C', 'T', 'Y', 'X'};
         public static string GetNextMarkAfter(string mark) // Выдаёт следующий знак в данной серии или создает следующую серию
         {
-            string nomer = mark.Substring(1, mark.Length - 3);
-            if(nomer == "999")
+            try
             {
-
+                string nextMark = "";
+                string nomer = mark.Substring(1, mark.Length - 6);
+                if (nomer == "999")
+                {
+                    nomer = "001";
+                    string seria = "" + mark[0] + mark[4] + mark[5];
+                    if (seria[2] != symbolsUsed[symbolsUsed.Count - 1])
+                    {
+                        int indexSymbol = -1;
+                        for (int i = 0; i < symbolsUsed.Count; i++)
+                        {
+                            if (symbolsUsed[i] == seria[2])
+                            {
+                                indexSymbol = i;
+                            }
+                        }
+                        nextMark = "" + seria[2] + nomer + seria[1] + symbolsUsed[indexSymbol + 1] + mark[mark.Length - 3] + mark[mark.Length - 2] + mark[mark.Length - 1];
+                    }
+                    else
+                    {
+                        if (seria[0] != symbolsUsed[symbolsUsed.Count - 1])
+                        {
+                            int indexSymbol = -1;
+                            for (int i = 0; i < symbolsUsed.Count; i++)
+                            {
+                                if (symbolsUsed[i] == seria[0])
+                                {
+                                    indexSymbol = i;
+                                }
+                            }
+                            nextMark = "" + symbolsUsed[indexSymbol + 1] + nomer + seria[1] + symbolsUsed[0] + mark[mark.Length - 3] + mark[mark.Length - 2] + mark[mark.Length - 1];
+                        }
+                        else
+                        {
+                            if (seria[1] != symbolsUsed[symbolsUsed.Count - 1])
+                            {
+                                int indexSymbol = -1;
+                                for (int i = 0; i < symbolsUsed.Count; i++)
+                                {
+                                    if (symbolsUsed[i] == seria[1])
+                                    {
+                                        indexSymbol = i;
+                                    }
+                                }
+                                nextMark = "" + symbolsUsed[0] + nomer + symbolsUsed[indexSymbol + 1] + symbolsUsed[0] + mark[mark.Length - 3] + mark[mark.Length - 2] + mark[mark.Length - 1];
+                            }
+                            else
+                            {
+                                nextMark = "error";
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    nomer = string.Format("{0:D3}", Convert.ToInt32(nomer) + 1);
+                    int j = 0;
+                    for (int i = 0; i < mark.Length; i++)
+                    {
+                        if (i == 1 || i == 2 || i == 3)
+                        {
+                            nextMark += nomer[j];
+                            j++;
+                        }
+                        else
+                        {
+                            nextMark += mark[i];
+                        }
+                    }
+                }
+                return nextMark;
             }
-            return "";
+            catch
+            {
+                return "error";
+            }
         }
         public static string GetNextMarkAfterInRange(string prevMark, string rangeStart, string rangeEnd) // Выдаёт следующий номер
         {
-
-            return "";
+            try
+            {
+                string kodRegion = prevMark.Substring(prevMark.Length - 3);
+                string kodRegionStart = rangeStart.Substring(rangeStart.Length - 3);
+                string kodRegionEnd = rangeEnd.Substring(rangeEnd.Length - 3);
+                if (kodRegionStart != kodRegionEnd || kodRegionStart != kodRegion)
+                {
+                    return "out of stock";
+                }
+                bool a = false; // Входит ли данное число в диапозон
+                string mark = rangeStart;
+                while (mark != rangeEnd)
+                {
+                    if (mark == prevMark)
+                    {
+                        a = true;
+                        break;
+                    }
+                    mark = GetNextMarkAfter(mark);
+                }
+                if (mark == prevMark)
+                {
+                    a = true;
+                }
+                string nextMark = prevMark;
+                if (a == true)
+                {
+                    if (nextMark != rangeEnd)
+                    {
+                        nextMark = GetNextMarkAfter(nextMark);
+                    }
+                    else
+                    {
+                        return "out of stock";
+                    }
+                }
+                else
+                {
+                    return "out of stock";
+                }
+                return nextMark;
+            }
+            catch
+            {
+                return "out of stock";
+            }
         }
-        public static int GetCombinationsCountInRange(string mark1, String mark2) // Расчёт оставшихся свободных номеров для региона
+        public static int GetCombinationsCountInRange(string mark1, string mark2) // Расчёт оставшихся свободных номеров для региона
         {
-            return 354;
+            try
+            {
+                string kodRegion1 = mark1.Substring(mark1.Length - 3);
+                string kodRegion2 = mark1.Substring(mark1.Length - 3);
+                if (kodRegion1 != kodRegion2)
+                {
+                    return 0;
+                }
+                int count = 0;
+                string nextMark = mark1;
+                count++;
+                while (nextMark != mark2)
+                {
+                    nextMark = GetNextMarkAfter(nextMark);
+                    count++;
+                }
+                return count;
+            }
+            catch
+            {
+                return 0;
+            }
         }
     }
 }
--- UnitTestRegMarkLib/UnitTests.cs
+++ UnitTestRegMarkLib/UnitTests.cs
@@ -24,5 +24,132 @@ namespace UnitTestRegMarkLib
             bool actual = Calculation.CheckMark(mark);
             Assert.IsFalse(actual);
         }
+        // Тест, на проверку, что метод который выдаёт следующий номер корректен
+        [TestMethod]
+        public void GetNextMarkAfter_Correctly()
+        {
+            string exception = "B001BA152";
+            string mark = "A999BX152";
+            string actual = Calculation.GetNextMarkAfter(mark);
+            Assert.AreEqual(exception, actual);
+        }
+        // Тест, на проверку, что метод который выдаёт следующий номерной знак, выводит результат значение которого не равно не правильному
+        [TestMethod]
+        public void GetNextMarkAfter_NotCorrectly()
+        {
+            string exception = "A006AA152";
+            string mark = "A004AA152";
+            string actual = Calculation.GetNextMarkAfter(mark);
+            Assert.IsFalse(exception == actual);
+        }
+        // Тест, на проверку, что метод который выдаёт следующий номер корректен
+        [TestMethod]
+        public void GetNextMarkAfter_NotNull()
+        {
+            string mark = "A210XT152";
+            string actual = Calculation.GetNextMarkAfter(mark);
+            Assert.IsNotNull(actual);
+        }
+        // Тест, на проверку, что метод для определения следующего номера в диапозоне высчитывает корректно
+        [TestMethod]
+        public void GetNextMarkAfterInRange_Correctly()
+        {
+            string exception = "A006AA152";
+            string prevMark = "A005AA152";
+            string rangeStart = "A001AA152";
+            string rangeEnd = "A800AB152";
+            string actual = Calculation.GetNextMarkAfterInRange(prevMark, rangeStart, rangeEnd);
+            Assert.IsTrue(exception == actual);
+        }
+        // Тест, на проверку, что метод для определения следующего номера в диапозоне высчитывает корректно и выводит результат с правильным типом данных
+        [TestMethod]
+        public void GetNextMarkAfterInRange_CorrectlyType()
+        {
+            string prevMark = "A005AA152";
+            string rangeStart = "A001AA152";
+            string rangeEnd = "A800AB152";
+            string actual = Calculation.GetNextMarkAfterInRange(prevMark, rangeStart, rangeEnd);
+            Assert.IsInstanceOfType(actual, typeof(string));
+        }
+        // Тест, на проверку, что метод который выдаёт количество возможных номеров в диапозоне
+        [TestMethod]
+        public void GetCombinationsCountInRange_Correctly()
+        {
+            int exception = 1009;
+            string mark1 = "A001AA152";
+            string mark2 = "A010AB152";
+            int actual = Calculation.GetCombinationsCountInRange(mark1, mark2);
+            Assert.AreEqual(exception, actual);
+        }
+        // Тест, на проверку, что метод который выдаёт количество возможных номеров в диапозоне, выводит результат значение которого не равно не правильному
+        [TestMethod]
+        public void GetCombinationsCountInRange_NotCorrectly()
+        {
+            int exception = 150;
+            string mark1 = "A001AA152";
+            string mark2 = "A010AA152";
+            int actual = Calculation.GetCombinationsCountInRange(mark1, mark2);
+            Assert.IsTrue(exception != actual);
+        }
+        // Тест, на проверку, что метод который выдаёт количество возможных номеров в диапозоне возвращает результат с верным типом
+        [TestMethod]
+        public void GetCombinationsCountInRange_TypeCorrectly()
+        {
+            string mark1 = "A001AA152";
+            string mark2 = "A010AB152";
+            int actual = Calculation.GetCombinationsCountInRange(mark1, mark2);
+            Assert.IsInstanceOfType(actual, typeof(int));
+        }
+
+
+        // Методы высокой сложности
+        // Тест, на проверку, что метод для определения корректности номера выдёт корректное значение если передано не правильное значение
+        [TestMethod]
+        public void CheckMark_CorrectlyType()
+        {
+            string mark = "A000AA152";
+            bool actual = Calculation.CheckMark(mark);
+            Assert.IsInstanceOfType(actual, typeof(bool));
+        }
+        // Тест, на проверку, что метод который выдаёт следующий номер корректно выводит результат, если все регистрационные знаки закончились
+        [TestMethod]
+        public void GetNextMarkAfter_Border()
+        {
+            string exception = "error";
+            string mark = "X999XX152";
+            string actual = Calculation.GetNextMarkAfter(mark);
+            Assert.IsTrue(exception == actual);
+        }
+        // Тест, на проверку, что метод для определения следующего номера в диапозоне выводит сообщение, что следующий номер не найден если разные регионы
+        [TestMethod]
+        public void GetNextMarkAfterInRange_NotCorrectlyRegion()
+        {
+            string exception = "out of stock";
+            string prevMark = "A005AA777";
+            string rangeStart = "A001AA155";
+            string rangeEnd = "A800AB155";
+            string actual = Calculation.GetNextMarkAfterInRange(prevMark, rangeStart, rangeEnd);
+            Assert.AreEqual(exception, actual);
+        }
+        // Тест, на проверку, что метод для определения следующего номера в диапозоне выводит сообщение, если предыдущий номер не входит в диапозон
+        [TestMethod]
+        public void GetNextMarkAfterInRange_NotCorrectly()
+        {
+            string exception = "out of stock";
+            string prevMark = "A990AB152";
+            string rangeStart = "A001AA152";
+            string rangeEnd = "A800AB152";
+            string actual = Calculation.GetNextMarkAfterInRange(prevMark, rangeStart, rangeEnd);
+            Assert.IsTrue(exception == actual);
+        }
+        // Тест, на проверку, что метод для определения корректности номера не выдаёт ошибку если неправильный регион
+        [TestMethod]
+        public void CheckMark_NotCorrectlyRegion()
+        {
+            bool exception = false;
+            string mark = "X345AT000";
+            bool actual = Calculation.CheckMark(mark);
+            Assert.AreEqual(exception, actual);
+        }
     }
 }
